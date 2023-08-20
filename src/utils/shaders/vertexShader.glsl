e1ecc84c6b26d28efe72bc379862c77858356304
gthnmp <gthn217@gmail.com>
2023-08-20 09:53:55 +0700

    Changed some uniforms and attributes

--- src/utils/shaders/vertexShader.glsl
+++ src/utils/shaders/vertexShader.glsl
@@ -3,22 +3,22 @@ attribute vec2 a_texCoord;
 attribute vec2 a_containerDimension;
 attribute vec2 a_mousePercentage;
 
-vec2 imageDimension = vec2(2200.0, 1080.0);
-vec2 containerDimension = vec2(0.35, 1.0);
-
-float imageAspect = imageDimension.x / imageDimension.y;
-
 uniform vec2 u_resolution;
 uniform float u_startingPositionPercentage;
+uniform vec2 u_imageDimension;
+uniform vec2 u_containerDimension;
 
 varying vec2 v_texCoord;
 
+float imageAspect = u_imageDimension.x / u_imageDimension.y;
+
 void main(){
+  gl_Position = vec4(a_position.x, a_position.y * -1.0, 1.0, 1.0);
+
   v_texCoord = vec2(
     a_texCoord.x / imageAspect, 
     a_texCoord.y * imageAspect
   );
   v_texCoord = v_texCoord / imageAspect;
-  v_texCoord.x += (containerDimension.x * 2.0) * u_startingPositionPercentage; 
-  gl_Position = vec4(a_position.x, a_position.y * -1.0, 1.0, 1.0);
+  v_texCoord.x += (u_containerDimension.x * 2.0) * u_startingPositionPercentage; 
 }
